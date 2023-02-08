79b68b44301d9447bb19e9ea3f105b0c5d045016
Tanvir Ahmed <tanvir.ahmed@restonlogic.com>
2023-02-07 21:47:11 -0500

    added prometheus helm install

---
+++ infrastructure/post-cluster-services/prometheus_helm.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  version    = "19.3.3"
+  namespace  = "monitoring"
+}
+
